ff97313693bafd647ea7fd7d0eae0d224170275d
Zen <64142347+zensdev@users.noreply.github.com>
2023-06-26 22:15:58 +0700

    change loadhd to loaihd

--- src/app/controllers/ContractController.js
+++ src/app/controllers/ContractController.js
@@ -97,7 +97,7 @@ class ContractController {
 
       const contract = await contractSchema.findById(_id)
         .populate("nhanvien", "hoten")
-        .populate("loadhd", "loaihd")
+        .populate("loaihd", "loaihd")
         .populate("khachhang", "name")
         .populate({
           path: 'donhang',
