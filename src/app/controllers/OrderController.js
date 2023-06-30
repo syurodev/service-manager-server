5712df33fd52bee82c8749e81ea49a90d25f4f6c
Zen <64142347+zensdev@users.noreply.github.com>
2023-06-30 11:55:42 +0700

    add filter get orders

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -121,7 +121,7 @@ class OrderController {
   //[GET] /api/order/
   async get(req, res) {
     try {
-      const { limit = 15, sort = "createAt", page = 1, nhanvien = "", deleted = false, khachhang = null, mini = false, q = "", role = "" } = req.query
+      const { limit = 15, sort = "createAt", page = 1, nhanvien = null, deleted = false, khachhang = null, mini = false, q = "", role = "" } = req.query
       const query = { deleted: deleted }
 
 
@@ -138,7 +138,7 @@ class OrderController {
       }
 
       if (khachhang) {
-        query.khachhang = { $regex: khachhang }
+        query.khachhang = khachhang
       }
 
       if (mini) {
