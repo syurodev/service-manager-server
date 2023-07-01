c737f9a9cbb80e7d2ed68d83ee16317318b3c9ca
Zen <64142347+zensdev@users.noreply.github.com>
2023-07-01 16:54:11 +0700

    get provinces

--- src/app/controllers/LocationController.js
+++ src/app/controllers/LocationController.js
@@ -1,4 +1,6 @@
 const provinceSchema = require("../models/Provinces")
+const districtsSchema = require("../models/Districts")
+const wardsSchema = require("../models/Wards")
 
 class LocationController {
   async getListProvince(req, res) {
@@ -15,6 +17,48 @@ class LocationController {
         res.status(201).json(provinces)
       } else {
         res.status(201).json(provinces)
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ error: "Internal Server Error" })
+    }
+  }
+
+  async getListDistricts(req, res) {
+    const { q } = req.query
+    const query = {}
+
+    if (q) {
+      query.name = { $regex: q, $options: "i" }
+    }
+
+    try {
+      const districts = await districtsSchema.find(query)
+      if (districts.length > 0) {
+        res.status(201).json(districts)
+      } else {
+        res.status(201).json(districts)
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ error: "Internal Server Error" })
+    }
+  }
+
+  async getListWards(req, res) {
+    const { q } = req.query
+    const query = {}
+
+    if (q) {
+      query.name = { $regex: q, $options: "i" }
+    }
+
+    try {
+      const wards = await wardsSchema.find(query)
+      if (wards.length > 0) {
+        res.status(201).json(wards)
+      } else {
+        res.status(201).json(wards)
       }
     } catch (error) {
       console.log(error)
--- src/routes/private/location.route.js
+++ src/routes/private/location.route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const LocationController = require("../../app/controllers/LocationController");
 
 router.get("/province", LocationController.getListProvince);
+router.get("/district", LocationController.getListDistricts);
+router.get("/ward", LocationController.getListWards);
 
 module.exports = router;
